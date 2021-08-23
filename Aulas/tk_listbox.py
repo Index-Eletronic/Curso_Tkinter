d45972b6a97d6207d16bf639c8c2f898d25c4a83
Fil Tuon <index.eletronic@gmail.com>
2021-08-23 15:34:14 -0300

    .

--- Aulas/tk_listbox.py
+++ Aulas/tk_listbox.py
@@ -1,13 +1,25 @@
 from tkinter import *
 
 janela = Tk()
+def selecao():
+    etq['text'] ='Local é: ' + lista.get(ANCHOR)
 
-
-lista = Listbox(janela)
+et2 = Label(janela, text='Qual estado você mora?')
+et2.pack()
+lista = Listbox(janela, width=50, height=5)
 lista.pack()
-lista.insert(END,'Teste 1')
-lista.insert(END,'Teste 2')
-lista.insert(END,'Teste 3')
+
+minhalista=['São Paulo','Rio de Janeio', 'Minas Gerais']
+for item in minhalista:
+    lista.insert(END, item) # Irá pegar os itens da lista e adiionar ao final
+
+bt1=Button(janela, text='Selecionado', command=selecao)
+bt1.pack()
+
+etq=Label(janela, text=" ")
+etq.pack()
+
 
 janela.geometry('500x400+50+200')
+janela.resizable(width=0, height=0)
 janela.mainloop()
